85b2e8f7ecf39af5d5d66c0b652a79cfc2716648
Bram De vroey <bramdevroey@gmail.com>
2021-03-30 09:19:36 +0200

    Add processed slider dataset to repository

--- get_data.py
+++ get_data.py
@@ -5,23 +5,19 @@ from preprocessing import extract_dataset, process_audio
 
 # All slider data
 # %%
-slider_data = pd.concat(
-    [
-        extract_dataset("/home/bram/datasets/0_dB_slider.zip"),
-        extract_dataset("/home/bram/datasets/-6_dB_slider.zip"),
-        extract_dataset("/home/bram/datasets/6_dB_slider.zip"),
-    ]
-)
-slider_data.reset_index(inplace=True, drop=True)
-slider_data
+slider_data = [
+    extract_dataset("/home/bram/datasets/0_dB_slider.zip"),
+    extract_dataset("/home/bram/datasets/-6_dB_slider.zip"),
+    extract_dataset("/home/bram/datasets/6_dB_slider.zip"),
+]
 
 # %%
-processed_slider = process_audio(slider_data, datadir="/home/bram/datasets")
+processed_slider = [
+    process_audio(slider_data_x, datadir="/home/bram/datasets")
+    for slider_data_x in slider_data
+]
 # %%
-processed_slider.to_csv("slider_all.csv.xz", index=False)
-
-# load from file
-# %%
-processed = pd.read_csv("./slider_all.csv.xz")
+processed_slider_all = pd.concat(processed_slider)
+processed_slider_all.to_csv("slider_all.csv.xz", index=False)
 
 # %%
