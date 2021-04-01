dff201403f86addf5819fb2d3a751cd1df3e365a
Bram De vroey <bramdevroey@gmail.com>
2021-04-01 15:44:08 +0200

    predict update

--- predict.py
+++ predict.py
@@ -7,11 +7,17 @@ from preprocessing import get_audio_features
 
 
 def predict_failure(sound, machine_type, model=None):
+    """Predict whether the provides (list of) sound files is functioning normally. 
 
+    :param sound: Sound file that will be analysed. Can also be a list of paths.
+    :param machine_type: Type of machine. One of "slider", "valve", "fan", "pump"
+    :param model: Optional, select pickled model file.
+    :return: Array with 0 or 1 (normal / abnormal) for each soundfile.
+    """
     # select and load trained model
     if model is None:
         if machine_type in ["fan", "slider", "pump", "valve"]:
-            model_path = Path(f"./saved_model/Predict_{machine_type}.sav")
+            model_path = Path(f"./saved_model/Predict_{machine_type}_sound_type.sav")
         else:
             raise RuntimeError("machine_type should be slider, fan, pump, or valve.")
     else:
@@ -25,33 +31,53 @@ def predict_failure(sound, machine_type, model=None):
     # load features from audio file
     if not hasattr(sound, "__iter__") or isinstance(sound, str):
         sound = [sound]
+
     features = [get_audio_features(soundfile) for soundfile in sound]
+
+    selected_features = (
+        [
+            "T_rms_mean",
+            "T_rms_std",
+            "T_zcr_mean",
+            "F_mel_mean",
+            "F_mel_std",
+            "F_mel_rms_mean",
+            "F_mel_rms_std",
+            "F_mfcc_mean",
+            "F_mfcc_std",
+            "F_flatness_mean",
+            "F_bandwidth_mean",
+            "F_bandwidth_std",
+            "F_contrast_mean",
+            "F_rolloff_mean",
+            "F_rolloff_std",
+        ]
+        if machine_type != "valve"
+        else [
+            "T_rms_mean",
+            "T_rms_std",
+            "T_zcr_mean",
+            "T_zcr_std",
+            "F_mel_mean",
+            "F_mel_std",
+            "F_mel_rms_mean",
+            "F_mel_rms_std",
+            "F_mfcc_mean",
+            "F_mfcc_std",
+            "F_flatness_mean",
+            "F_flatness_std",
+            "F_bandwidth_mean",
+            "F_bandwidth_std",
+            "F_contrast_mean",
+            "F_contrast_std",
+            "F_rolloff_mean",
+            "F_rolloff_std",
+        ]
+    )
 
     # funnel features into same order as model
     X = np.array(
-        [
-            [
-                feature_i[name]
-                for name in [
-                    "T_rms_mean",
-                    "T_rms_std",
-                    "T_zcr_mean",
-                    "F_mel_mean",
-                    "F_mel_std",
-                    "F_mel_rms_mean",
-                    "F_mel_rms_std",
-                    "F_mfcc_mean",
-                    "F_mfcc_std",
-                    "F_flatness_mean",
-                    "F_bandwidth_mean",
-                    "F_bandwidth_std",
-                    "F_contrast_mean",
-                    "F_rolloff_mean",
-                    "F_rolloff_std",
-                ]
-            ]
-            for feature_i in features
-        ]
+        [[feature_i[name] for name in selected_features] for feature_i in features]
     )
 
     # make prediction
